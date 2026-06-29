493a64b05af11d2695887852438cda37ec2971b4
Rahul Verma <vermarahul72787@gmail.com>
2026-06-30 03:14:23 +0530

    Final

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/lib/auth";
-import { cookies } from "next/headers";
 import {
   ArrowRight,
   GitBranch,
